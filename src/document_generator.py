00f9b8bd7e96e172796805b563c3afe04fa00ec3
Max Smagin <lazlogogalac777@gmail.com>
2023-07-19 19:03:50 +0300

    Switched to constructor args from setters in gen

--- src/document_generator.py
+++ src/document_generator.py
@@ -1,9 +1,8 @@
 from typing import Callable
 from collections.abc import Generator
 
-class DocumentGenerator():
-    def __init__(self) -> None:
-        self.header: str = r"""\documentclass[11pt]{report}
+class DocumentGenerator:
+    DEFAULT_HEADER: str = r"""\documentclass[11pt]{report}
 \usepackage[T2A]{fontenc}
 \usepackage[utf8]{inputenc}
 \usepackage[russian]{babel}
@@ -17,70 +16,42 @@ class DocumentGenerator():
 \begin{document}
 
 \pagestyle{empty}"""
-        self.border: str = r"""\begin{center}
+
+    DEFAULT_BORDER: str = r"""\begin{center}
 \noindent\rule{8cm}{0.4pt}
-\end{center}
-"""
-        self.title: str = "Индивидуальное задание."
-        self.description: str = ""
-        self.variant_generator: Callable[[int, bool], str] = lambda variant_n, is_solution: ""
-        self.variant_enumerator: Callable[[int], str] = lambda variant_n: f"Вариант ${variant_n}$"
+\end{center}"""
 
-    """
-    Sets 'header' part of the document (i. e. where '\\usepackage' is defined)
-    """
-    def set_header(self, header: str):
-        self.header = header
-        return self
-
-    """
-    Sets the title of the document.
-    """
-    def set_title(self, title: str):
-        self.title = title
-        return self
-
-    """
-    Sets the description of the document.
-    """
-    def set_description(self, description: str):
-        self.description = description
-        return self
-
-    """
-    Sets the variant generator function.
-    Variant generator receives a variant number, a boolean value, indicating if this is a solution, and returns a string.
-    """
-    def set_variant_generator(self, variant_generator: Callable[[int, bool], str]):
+    def __init__(self, 
+                 variant_generator: Callable[[int], tuple[str, str]], 
+                 description: str,
+                 header: str = DEFAULT_HEADER, 
+                 border: str = DEFAULT_BORDER,
+                 title: str = "Индивидуальное задание.",
+                 variant_enumerator: Callable[[int], str] = lambda variant_n: f"Вариант ${variant_n}$"):
         self.variant_generator = variant_generator
-        return self
-
-    """
-    Sets the border of the document, which divides variants from each other.
-    """
-    def set_border(self, border: str):
+        self.description = description
+        self.header = header
         self.border = border
-        return self
-
-    def set_variant_enumerator(self, variant_enumerator: Callable[[int], str]):
+        self.title = title
         self.variant_enumerator = variant_enumerator
-        return self
 
-    def generate(self, variant_count: int, is_solution: bool) -> Generator[str]:
-        def line(text: str = ""):
-            yield text + "\n"
+    def generate(self, variant_count: int) -> Generator[tuple[str, str]]:
+        def line(task: str = "", solution: str = None) -> tuple[str, str]:
+            if solution is None:
+                solution = task
+            return task + "\n", solution + "\n"
 
-        line(self.header)
-        line(r"{\bf " + self.title + "}")
-        line()
-        line(self.description)
-        line()
+        yield line(self.header)
+        yield line(r"{\bf " + self.title + "}")
+        yield line()
+        yield line(self.description)
+        yield line()
         
         for variant_n in range(variant_count):
-            line(self.border)
-            line(self.variant_enumerator(variant_n))
-            line(self.variant_generator(variant_n, is_solution))
+            yield line(self.border)
+            yield line(self.variant_enumerator(variant_n))
+            yield line(*self.variant_generator(variant_n))
 
-        line()
-        line(r"\end{document}")
+        yield line()
+        yield line(r"\end{document}")
 
